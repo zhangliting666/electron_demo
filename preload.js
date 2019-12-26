a32f74e3010cb3180426e912637f9195160b2472
zlt <15055170344@163.com>
2019-12-26 15:29:05 +0800

    first commit

--- preload.js
+++ preload.js
@@ -1,10 +1,11 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+//所有Node.js API在预加载过程中都可用。
+//它具有与Chrome扩展程序相同的沙箱。
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
   }
+
 
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
